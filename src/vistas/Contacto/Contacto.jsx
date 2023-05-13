62901d882afc35767d70e040b1bbceb5b42a2ab8
Julian <julianlopez4301@gmail.com>
2023-05-13 17:59:50 -0300

    Le falta colores y puede enviar datos

--- src/vistas/Contacto/Contacto.jsx
+++ src/vistas/Contacto/Contacto.jsx
@@ -1,7 +1,64 @@
-import React from 'react'
-
+import React from "react";
+import "./Contacto.css";
 export default function Contacto() {
+  const [datos, setDatos] = React.useState({
+    nombre: "",
+    email: "",
+    descripcion: "",
+  });
+
+  const handlerForm = (e) => {
+    e.preventDefault();
+    setDatos({
+      ...datos,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setDatos({
+      nombre: "",
+      email: "",
+      descripcion: "",
+    });
+  };
   return (
-    <div>Contacto</div>
-  )
+    <div className="container-form">
+      <h2>¡Contactemonos!</h2>
+      <form className="form" onSubmit={submitHandler}>
+        <div className="container-span-input">
+          <span className="span-contacto">Nombre</span>
+          <input
+            type="text"
+            name="nombre"
+            onChange={handlerForm}
+            className="input-contacto"
+            value={datos.nombre}
+          />
+        </div>
+        <div className="container-span-input">
+          <span className="span-contacto">Email</span>
+          <input
+            type="email"
+            name="email"
+            onChange={handlerForm}
+            className="input-contacto"
+            value={datos.email}
+          />
+        </div>
+        <div className="container-span-input">
+          <span className="span-contacto">Descripcion</span>
+          <textarea
+            name="descripcion"
+            onChange={handlerForm}
+            className="input-contacto"
+            rows={4}
+            value={datos.descripcion}
+          />
+        </div>
+        <button className="button-contacto">Enviar mensaje</button>
+      </form>
+    </div>
+  );
 }
