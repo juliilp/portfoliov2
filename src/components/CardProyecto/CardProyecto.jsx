30cb86cfd969af1b156fdb6b35796b6e37fb118d
Julian <julianlopez4301@gmail.com>
2023-05-16 18:50:06 -0300

    agregue cosas

--- src/components/CardProyecto/CardProyecto.jsx
+++ src/components/CardProyecto/CardProyecto.jsx
@@ -1,21 +1,12 @@
 import React from "react";
 import "./CardProyecto.css";
-export default function CardProyecto({
-  imagen,
-  titulo,
-  texto,
-  tecnologias,
-  pagina,
-  repo,
-}) {
+export default function CardProyecto({ imagen, titulo, texto, pagina, repo }) {
   return (
     <div className="card-container">
       <img src={imagen} alt={imagen} className="card-imagen" />
       <div className="card-info">
         <h3 className="card-titulo">{titulo}</h3>
         <p className="card-texto">{texto}</p>
-        <h3 className="card-titletecnologias">Tecnologías usadas</h3>
-        <div className="card-tecnologias">{tecnologias}</div>
         <div className="card-container-buttons">
           {" "}
           <a href={pagina} target="blank" className="card-buttons">
