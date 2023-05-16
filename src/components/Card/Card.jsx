e3b314f5dbf858d7e8b8f000e1330a1daa0680d9
Julian <julianlopez4301@gmail.com>
2023-05-16 18:49:15 -0300

    agregue card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,5 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Card.css";
-export default function Card() {
-  return <div>Card</div>;
+import MercadoSolidario from "../../assets/mercadosolidario/mercadosolidario1351.png";
+import FoodApp from "../../assets/foodapp/foodapp1366.png";
+import VideoGames from "../../assets/videogamesapp/videogames1345.png";
+import Gigabyte from "../../assets/gigabyteapp/gigabyte1351.png";
+import Menu from "./Menu/Menu";
+import { storeMenu } from "../../store/store";
+const arrayImagenes = [
+  { imagen: MercadoSolidario },
+  { imagen: FoodApp },
+  { imagen: VideoGames },
+  { imagen: Gigabyte },
+];
+export default function Card({ titulo, textoMenu }) {
+  const [active, setActive] = useState(false);
+
+  const handlerActivate = () => {
+    setActive(!active);
+  };
+
+  const buttonHandler = () => {
+    setActive(!active);
+  };
+  return (
+    <div className="card">
+      <h3>{titulo}</h3>
+      <img src={MercadoSolidario} alt="Imagen" className="imagen-carrousel" />
+      <button className="button-card" onClick={handlerActivate}>
+        Ver más
+      </button>
+      {active === true ? (
+        <div className="container-menu">
+          <Menu onClickButton={buttonHandler} texto={textoMenu} />
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
+  );
 }
