935a35d79a2f92802f55e953529c93a93f0a9244
Julian <julianlopez4301@gmail.com>
2023-05-19 14:09:07 -0300

    Footer mobile terminado

--- src/vistas/Footer/Footer.jsx
+++ src/vistas/Footer/Footer.jsx
@@ -3,14 +3,9 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <footer className="footer">
-      <h3>Julian</h3>
-      <ul className="footer-ul">
-        <li>Lorem ipsum dolor </li>
-        <li>Lorem ipsum dolor </li>
-        <li>Lorem ipsum dolor </li>
-        <li>Lorem ipsum dolor </li>
-        <li>Lorem ipsum dolor </li>
-      </ul>
+      <h6 className="title-footer">
+        © 2023 Julian Lopez Padua™. All Rights Reserved.
+      </h6>
     </footer>
   );
 }
