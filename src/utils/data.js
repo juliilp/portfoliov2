d39486a3e7332e28e7e34cbc3750a8f159b244a4
Julian <julianlopez4301@gmail.com>
2023-05-22 16:03:44 -0300

    Corrección imagenes

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,4 +1,4 @@
-import MercadoSolidario from "../assets/mercadosolidario/mercadosolidario762.png";
+import MercadoSolidario from "../assets/mercadosolidario/mercadosolidario.png";
 import FoodApp from "../assets/foodapp/foodapp1366.png";
 import VideoGames from "../assets/videogamesapp/videogames1345.png";
 import Gigabyte from "../assets/gigabyteapp/gigabyte1351.png";
@@ -8,21 +8,29 @@ const data = [
     name: "Mercado Solidario",
     imagen: MercadoSolidario,
     texto: "Soy texto asdasd",
+    web: "https://mercadosolidario.vercel.app/",
+    github: "https://github.com/juliilp/mercadosolidario",
   },
   {
     name: "Food App",
     imagen: FoodApp,
     texto: "texto2",
+    web: "https://project-food-two.vercel.app/",
+    github: "https://github.com/juliilp/Project-Food",
   },
   {
     name: "Videogames app",
     imagen: VideoGames,
     texto: "texto3",
+    web: "https://videogames-pi-lyart.vercel.app/",
+    github: "https://github.com/juliilp/Project-Videogames",
   },
   {
     name: "Gygabite",
     imagen: Gigabyte,
     texto: "texto4",
+    web: "https://gigabyte-gallery.vercel.app/",
+    github: "https://github.com/juliilp/Gigabyte-Gallery",
   },
 ];
 
