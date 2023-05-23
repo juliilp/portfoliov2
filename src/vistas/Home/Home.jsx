f1944f981c34557a4f684b10473ff84a4d08be7d
Julian <julianlopez4301@gmail.com>
2023-05-23 18:58:37 -0300

    Css

--- src/vistas/Home/Home.jsx
+++ src/vistas/Home/Home.jsx
@@ -15,33 +15,36 @@ export default function Home() {
         />
       </picture>
       <div className="container-home-info">
-        <h1 className="home-nombre">¡Hola, soy Julian!</h1>
-        <h2 className="home-subnombre">Desarollador Web Full Stack</h2>
-        <div className="home-containers-icons">
-          <a href="https://github.com/juliilp" target="_blank">
-            <i className="bi bi-github github" />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/julian-lopez-7b9746234/"
-            target="_blank"
-          >
-            <i className="bi bi-linkedin linkedin" />
-          </a>
-          <a href="https://wa.me/1123820130" target="_blank">
-            <i className="bi bi-whatsapp whatsapp " />
-          </a>
-        </div>
-        <div className="container-home-buttons">
-          <a href="#contacto">
-            <button className="home-button">
-              Contacto <i className="bi bi-person-lines-fill icon-contacto " />
-            </button>
-          </a>
-          <a href={CV} target="_blank">
-            <button className="home-button">
-              Ver CV <i className="bi bi-eye-fill icon-cv " />
-            </button>
-          </a>
+        <div className="home-info-hijo">
+          <h1 className="home-nombre">¡Hola, soy Julian!</h1>
+          <h2 className="home-subnombre">Desarollador Web Full Stack</h2>
+          <div className="home-containers-icons">
+            <a href="https://github.com/juliilp" target="_blank">
+              <i className="bi bi-github github" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/julian-lopez-7b9746234/"
+              target="_blank"
+            >
+              <i className="bi bi-linkedin linkedin" />
+            </a>
+            <a href="https://wa.me/1123820130" target="_blank">
+              <i className="bi bi-whatsapp whatsapp " />
+            </a>
+          </div>
+          <div className="container-home-buttons">
+            <a href="#contacto">
+              <button className="home-button">
+                Contacto{" "}
+                <i className="bi bi-person-lines-fill icon-contacto " />
+              </button>
+            </a>
+            <a href={CV} target="_blank">
+              <button className="home-button">
+                Ver CV <i className="bi bi-eye-fill icon-cv " />
+              </button>
+            </a>
+          </div>
         </div>
       </div>
     </div>
