1b556e89c3c765abed6ae9469ea7141af05a2530
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-10-14 19:41:00 -0300

    pajaros caidos

--- src/vistas/Proyectosv2/Proyectosv2.jsx
+++ src/vistas/Proyectosv2/Proyectosv2.jsx
@@ -1,94 +1,39 @@
-import React from "react";
 import "./Proyectosv2.css";
+import dataProyectos from "../../utils/dataProyectos";
 import CardProyectov2 from "../../components/CardProyectov2/CardProyectov2";
 import CardProyectov2Inverse from "../../components/CardProyectov2Inverse/CardProyectov2Inverse";
 import { Section } from "../../App";
-import {
-  Mercado,
-  VideoGames,
-  Gigabyte,
-  FoodApp,
-  VeggieBudiness,
-  AppEcommerce,
-} from "../../utils/dataProyectos";
 export default function Proyectosv2() {
   return (
     <section className="section-proyectov2" id="proyectos">
       <h2 className="section-proyectov2-title">Proyectos</h2>
-      <div className="Proyectos">
-        {
-          <Section>
-            <CardProyectov2
-              image={Mercado.image}
-              titulo={Mercado.titulo}
-              texto={Mercado.texto}
-              github={Mercado.github}
-              pagina={Mercado.pagina}
-              tecnologias={Mercado.tecnologias}
-            />
-          </Section>
-        }
-        {
-          <Section>
-            <CardProyectov2Inverse
-              image={VideoGames.image}
-              titulo={VideoGames.titulo}
-              texto={VideoGames.texto}
-              github={VideoGames.github}
-              pagina={VideoGames.pagina}
-              tecnologias={VideoGames.tecnologias}
-            />
-          </Section>
-        }
-        {
-          <Section>
-            <CardProyectov2
-              image={Gigabyte.image}
-              titulo={Gigabyte.titulo}
-              texto={Gigabyte.texto}
-              github={Gigabyte.github}
-              pagina={Gigabyte.pagina}
-              tecnologias={Gigabyte.tecnologias}
-            />
-          </Section>
-        }
-        {
-          <Section>
-            <CardProyectov2Inverse
-              image={FoodApp.image}
-              titulo={FoodApp.titulo}
-              texto={FoodApp.texto}
-              github={FoodApp.github}
-              pagina={FoodApp.pagina}
-              tecnologias={FoodApp.tecnologias}
-            />
-          </Section>
-        }
-        {
-          <Section>
-            <CardProyectov2
-              image={VeggieBudiness.image}
-              titulo={VeggieBudiness.titulo}
-              texto={VeggieBudiness.texto}
-              github={VeggieBudiness.github}
-              pagina={VeggieBudiness.pagina}
-              tecnologias={VeggieBudiness.tecnologias}
-            />
-          </Section>
-        }
-        {
-          <Section>
-            <CardProyectov2Inverse
-              image={AppEcommerce.image}
-              titulo={AppEcommerce.titulo}
-              texto={AppEcommerce.texto}
-              github={AppEcommerce.github}
-              pagina={AppEcommerce.pagina}
-              tecnologias={AppEcommerce.tecnologias}
-            />
-          </Section>
-        }
-      </div>
+      <article className="Proyectos">
+        {dataProyectos.map((p, key) => {
+          return (
+            <Section key={key}>
+              {p.reverse === false ? (
+                <CardProyectov2
+                  image={p.image}
+                  titulo={p.titulo}
+                  texto={p.texto}
+                  github={p.github}
+                  pagina={p.pagina}
+                  tecnologias={p.tecnologias}
+                />
+              ) : (
+                <CardProyectov2Inverse
+                  image={p.image}
+                  titulo={p.titulo}
+                  texto={p.texto}
+                  github={p.github}
+                  pagina={p.pagina}
+                  tecnologias={p.tecnologias}
+                />
+              )}
+            </Section>
+          );
+        })}
+      </article>
     </section>
   );
 }
